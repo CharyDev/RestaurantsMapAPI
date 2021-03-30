e262a7f18d3d8b9281371e08f0fd67acd526569d
Chariya Puenpha <81368333+CharyDev@users.noreply.github.com>
2021-03-30 23:12:14 +0700

    upadate app

--- app.js
+++ app.js
@@ -6,7 +6,45 @@ function initMap() {
         zoom:15
     };  
     
-     map = new google.maps.Map(document.getElementById('map'), position);  
+    //map = new google.maps.Map(document.getElementById('map'), position);  
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: position,
+            zoom: 15
+        });
+    
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+            location : position,
+            radius : 5500,
+            type : ["restaurant"]
+        }, callback);
+    }
+    
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+    
+            var marker, i;
+    
+            for (i = 0; i < results.length; i++) {  
+                marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(results[i].geometry.location.lat(), results[i].geometry.location.lng()),
+                  map: map
+                });
+    
+                google.maps.event.addListener( marker, 'click', ( 
+                    function( marker, i ) {
+                        return function() {
+                            var infowindow = new google.maps.InfoWindow();
+                            infowindow.setContent(results[i].name);
+                            infowindow.open(map, marker);
+                        }
+                    }
+                )(marker, i));
+            }
+        }
+    }
 
     //Create search box from index.html
     const input = document.getElementById('search');       
